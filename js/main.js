9438bf96e292b3b86e5c2dd319cdb4ab36e695dd
Tom Nguyen <tomn98@live.unc.edu>
2021-04-28 10:15:31 -0400

    Added test button

--- js/main.js
+++ js/main.js
@@ -2,12 +2,17 @@ import Simulator from "./simulator.js";
 import Render from "./render.js"
 
 function start() {
+    
+};
+
+$(function() {
+    // start();  
     const strandCast = new Simulator();
     const view = new Render($("#root"), strandCast);
 
     strandCast.addPeer({type: "peer", id: "Peer_A"});
     strandCast.addPeer({type: "peer", id: "Peer_B"});
-    // strandCast.addPeer({type: "peer", id: "Peer_C"});
+    strandCast.addPeer({type: "peer", id: "Peer_C"});
     // strandCast.addPeer({type: "peer", id: "Peer_D"});
     // strandCast.addPeer({type: "peer", id: "Peer_E"});
     // strandCast.addPeer({type: "peer", id: "Peer_F"});
@@ -15,19 +20,21 @@ function start() {
     
     view.initView();
 
-    // console.log(strandCast.getSource().print()); 
-    // strandCast.getPeers().forEach((peer) => {
-    //     console.log(peer.print());
-    // })
-    // console.log(strandCast.getSink().print()); 
+    console.log(strandCast.getSource().print()); 
+    strandCast.getPeers().forEach((peer) => {
+        console.log(peer.print());
+    })
+    console.log(strandCast.getSink().print()); 
 
-    // strandCast.getLinks().forEach((link) => {
-    //     console.log(link.print());
-    // })
-};
+    strandCast.getLinks().forEach((link) => {
+        console.log(link.print());
+    })
 
-$(function() {
-    start();  
-
+    $("button").click(() => {
+        strandCast.addPeer({type: "peer", id: "Peer_D"});
+        strandCast.startStrand();
+        view.initView();
+        // alert("ADDING");
+    })
     
 });
